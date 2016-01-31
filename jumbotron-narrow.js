8159dd8843b7ef614c6140a6e91a0ec81da67137
repsaj2000 <repsaj2000@hotmail.com>
2016-01-31 18:52:08 +0100

    Update jumbotron-narrow.js

--- jumbotron-narrow.js
+++ jumbotron-narrow.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
     if($(document).width() < 768) {
-        $('.cool').addClass('container');
-        $('.cool').removeClass('limit400');
+        $('#cool').addClass('container');
+        $('#cool').removeClass('limit400');
+        $('#2cool').addClass('container');
+        $('#2cool').removeClass('limit400');
     }else{
-        $('.cool').addClass('limit400');
-        $('.cool').removeClass('container');
+        $('#cool').addClass('limit400');
+        $('#cool').removeClass('container');
+        $('#2cool').addClass('limit400');
+        $('#2cool').removeClass('container');
     }
 })
